2ec133b4f902b6d74922df36c734a6247fde2137
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-03-03 15:30:55 +0900

    取得した注文テーブルに応じてページを遷移させる処理を追加

--- src/main/java/jp/co/example/ecommerce_b/controller/OrderController.java
+++ src/main/java/jp/co/example/ecommerce_b/controller/OrderController.java
@@ -1,7 +1,13 @@
 package jp.co.example.ecommerce_b.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.example.ecommerce_b.domain.Order;
+import jp.co.example.ecommerce_b.service.OrderService;
 
 /**
  * 
@@ -11,8 +17,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/order")
 public class OrderController {
+	@Autowired
+	private OrderService orderService;
+	
 	@RequestMapping("/confirm")
-	public String showOrderConfirm() {
+	public String showOrderConfirm(Model model) {
+		Order order = orderService.showCart(1, 0);
+		if (order == null) {
+			return "item_curry";
+		}
+		model.addAttribute("order", order);
+		
 		return "order_confirm";
 	}
 }

32dc6e39d05a9e68a2bbbcb4d9b8eb9da01b2c5d
someyahirofumi <hirofumi.someya@rakus-partners.co.jp>
2022-03-03 11:23:15 +0900

    コントローラークラスにtoItemDetailメソッドを作成

--- src/main/java/jp/co/example/ecommerce_b/controller/EcController.java
+++ src/main/java/jp/co/example/ecommerce_b/controller/EcController.java
@@ -1,7 +1,13 @@
 package jp.co.example.ecommerce_b.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.example.ecommerce_b.domain.Item;
+import jp.co.example.ecommerce_b.domain.Topping;
 
 @Controller
 @RequestMapping("")
@@ -10,6 +16,20 @@ public class EcController {
 	@RequestMapping("")
 	public String index() {
 		return "login";
+	}
+	
+	
+	@RequestMapping("/toItemDetail")
+	public String toItemDetail(Integer itemId,Model model) {
+		Item item = service.findByItemId(itemId);
+		List<Topping> toppingList = service.findAll();
+		item.setToppingList(toppingList);
+		model.addAttribute("item",item);
+		return"item_detail";
+		
+		
+		
+		
 	}
 
 }

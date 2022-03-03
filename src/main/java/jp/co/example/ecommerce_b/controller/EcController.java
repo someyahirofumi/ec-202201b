74e5a6012712e3eddc5d2064f758f4f968cf4eb0
someyahirofumi <hirofumi.someya@rakus-partners.co.jp>
2022-03-03 12:53:01 +0900

    商品詳細画面表示機能を追加

--- src/main/java/jp/co/example/ecommerce_b/controller/EcController.java
+++ src/main/java/jp/co/example/ecommerce_b/controller/EcController.java
@@ -2,16 +2,21 @@ package jp.co.example.ecommerce_b.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.example.ecommerce_b.domain.Item;
 import jp.co.example.ecommerce_b.domain.Topping;
+import jp.co.example.ecommerce_b.service.Itemservice;
 
 @Controller
 @RequestMapping("")
 public class EcController {
+	
+	@Autowired 
+	private Itemservice service;
 	
 	@RequestMapping("")
 	public String index() {
@@ -21,10 +26,16 @@ public class EcController {
 	
 	@RequestMapping("/toItemDetail")
 	public String toItemDetail(Integer itemId,Model model) {
-		Item item = service.findByItemId(itemId);
-		List<Topping> toppingList = service.findAll();
+		System.out.println("システム起動");
+//		Item item = service.findByItemId(itemId);
+		Item item = service.findByItemId(1);
+		
+		List<Topping> toppingList = service.toppingFindAll();
 		item.setToppingList(toppingList);
 		model.addAttribute("item",item);
+//		System.out.println(item);
+//		System.out.println(toppingList);
+//		
 		return"item_detail";
 		
 		
--- src/main/java/jp/co/example/ecommerce_b/repository/Itemrepository.java
+++ src/main/java/jp/co/example/ecommerce_b/repository/Itemrepository.java
@@ -1,9 +1,17 @@
 package jp.co.example.ecommerce_b.repository;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
+
+import jp.co.example.ecommerce_b.domain.Item;
+import jp.co.example.ecommerce_b.domain.Topping;
 
 
 @Repository
@@ -11,6 +19,25 @@ public class Itemrepository {
 	
 	@Autowired
 	private NamedParameterJdbcTemplate template;
+	
+	private static final RowMapper<Item> ITEM_ROW_MAPPER=new BeanPropertyRowMapper<>(Item.class);
+	private static final RowMapper<Topping> TOPPING_ROW_MAPPER=new BeanPropertyRowMapper<>(Topping.class);
+	
+	
+	public Item findByItemId(Integer itemId) {
+		
+		String sql = "SELECT id,name,description,price_m,price_l,image_path FROM items WHERE id=:itemId;";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("itemId", itemId);
+		Item item = template.queryForObject(sql, param, ITEM_ROW_MAPPER);
+		return item;
+		
+	}
+	
+	public List<Topping> findAll(){
+		String sql = "SELECT id,name,price_m,price_l FROM toppings ; ";
+		List<Topping> list=template.query(sql, TOPPING_ROW_MAPPER);
+		return list;
+	}
 	
 	
 

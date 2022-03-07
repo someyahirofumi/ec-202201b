1a326d068b1a57228d878e42c15d45e3f6129ed8
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-03-07 12:01:07 +0900

    メール送信処理をOrderServiceに追記

--- src/main/java/jp/co/example/ecommerce_b/service/OrderService.java
+++ src/main/java/jp/co/example/ecommerce_b/service/OrderService.java
@@ -1,9 +1,16 @@
 package jp.co.example.ecommerce_b.service;
 
+import java.text.SimpleDateFormat;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
 import jp.co.example.ecommerce_b.domain.Order;
+import jp.co.example.ecommerce_b.domain.OrderItem;
 import jp.co.example.ecommerce_b.repository.OrderRepository;
 
 /**
@@ -15,6 +22,9 @@ import jp.co.example.ecommerce_b.repository.OrderRepository;
 public class OrderService {
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private MailSender sender;
 	/**
 	 * 
 	 * @param userId ユーザーID
@@ -31,5 +41,34 @@ public class OrderService {
 	 */
 	public void orderUpdate(Order order) {
 		orderRepository.update(order);
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日 hh時");
+		String stringPaymentMethod = null;
+		if (order.getPaymentMethod() == 1) {
+			stringPaymentMethod = "代金引換";
+		} else if(order.getPaymentMethod() == 2) {
+			stringPaymentMethod = "クレジットカード";
+		}
+		String text = "ご注文金額:" + String.format("%,d", order.getTotalPrice()) + "円\n"
+				+ "お支払い方法:" + stringPaymentMethod + "\n"
+				+ "配達日時:" + sdf.format(order.getDelivaryTime()) + "\n"
+				+ "ご注文内容:\n";
+		for (OrderItem orderItem : order.getOrderItemList()) {
+			text += orderItem.getItem().getName() + " " + orderItem.getQuantity() + "個\n";
+		}
+		
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom("pupi.yh@gmail.com");
+		message.setTo("pupi.yh@gmail.com");
+		//message.setTo(order.getDestinationEmail());
+		message.setSubject("ご注文を承りました。");
+		message.setText(text);
+		try {
+			sender.send(message);
+		} catch (MailException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		System.out.println("メールを送信しました");
 	}
 }

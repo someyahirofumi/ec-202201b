e7f33d3dc3c2f39cbe1e2bb4c3a2322deb99691f
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-03-08 15:01:32 +0900

    insert処理のControllerまで記述

--- src/main/java/jp/co/example/ecommerce_b/controller/FavoriteController.java
+++ src/main/java/jp/co/example/ecommerce_b/controller/FavoriteController.java
@@ -1,7 +1,12 @@
 package jp.co.example.ecommerce_b.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.example.ecommerce_b.service.FavoriteService;
 
 /**
  * 
@@ -10,6 +15,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 @RequestMapping("/favorite")
-public class FvoriteController {
+public class FavoriteController {
+	@Autowired
+	private FavoriteService favoriteService;
 	
+	@Autowired
+	private HttpSession session;
+	
+	@RequestMapping("/add")
+	public String favorite(Integer itemId) {
+		Integer userId = (Integer) session.getAttribute("userId");
+		favoriteService.favorite(userId, itemId);
+		
+		return "favolite_list";
+	}
 }
--- src/main/java/jp/co/example/ecommerce_b/service/FavoriteService.java
+++ src/main/java/jp/co/example/ecommerce_b/service/FavoriteService.java
@@ -1,10 +1,21 @@
 package jp.co.example.ecommerce_b.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import jp.co.example.ecommerce_b.repository.FavoriteRepository;
+
 /**
  * 
  * @author hondayuki
  *
  */
+@Service
 public class FavoriteService {
-
+	@Autowired
+	private FavoriteRepository favoriteRepository;
+	
+	public void favorite(Integer userId, Integer itemId) {
+		favoriteRepository.insert(userId, itemId);
+	}
 }

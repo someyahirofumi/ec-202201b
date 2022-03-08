0872957d068633e2c8dd4cc141d666efb1f14174
TaichiYazaki <taichi.yazaki@rakus-partners.co.jp>
2022-03-08 16:02:51 +0900

    検索結果数の表示

--- src/main/java/jp/co/example/ecommerce_b/service/Itemservice.java
+++ src/main/java/jp/co/example/ecommerce_b/service/Itemservice.java
@@ -14,62 +14,66 @@ import jp.co.example.ecommerce_b.repository.Itemrepository;
 @Service
 @Transactional
 public class Itemservice {
-	
+
 	@Autowired
 	private Itemrepository itemRepository;
+
 	/**
 	 * 全件検索 全商品一覧の表示
 	 */
-	public List<Item> findAllItemList(){
+	public List<Item> findAllItemList() {
 		return itemRepository.findAllItemList();
 	}
-	
+
 	/**
-	 * 全件検索　安い順
+	 * 全件検索 安い順
 	 */
-	public List<Item> lowList(){
+	public List<Item> lowList() {
 		return itemRepository.findAllLow();
 	}
-	
+
 	/**
-	 * 全件検索　安い順
+	 * 全件検索 安い順
 	 */
-	public List<Item> highList(){
+	public List<Item> highList() {
 		return itemRepository.findAllHigh();
 	}
-	
+
 	/**
 	 * 商品検索
 	 */
 	public List<Item> search(String name) {
 		return itemRepository.search(name);
 	}
-	
+
 	/**
-	 * 検索結果　安い順
+	 * 検索結果 安い順
 	 */
 	public List<Item> search1(String name) {
 		return itemRepository.search1(name);
 	}
 
-
-	
-	/**　送られてきたitemIdを条件としてItemテーブルを検索する
+	/**
+	 * 送られてきたitemIdを条件としてItemテーブルを検索する
+	 * 
 	 * @param itemId
-	 * @return　itemRepositoryのfindByItemIdメソッド実行する
+	 * @return itemRepositoryのfindByItemIdメソッド実行する
 	 */
 	public Item findByItemId(Integer itemId) {
-		Item item=itemRepository.findByItemId(itemId);
+		Item item = itemRepository.findByItemId(itemId);
 		return item;
 	}
-	
+
 	/**
-	 * @return　全トッピングリスト
+	 * @return 全トッピングリスト
 	 * 
 	 */
-	public List<Topping> toppingFindAll(){
+	public List<Topping> toppingFindAll() {
 		return itemRepository.findAll();
-		
+
 	}
 
+	public Integer searchCount(String name) {
+	return itemRepository.searchCount(name);
+	}
 }
